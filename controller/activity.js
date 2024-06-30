0cab1b7eb6cd3daa447040fdc0f07099f1630a1b
xiaoxu <innocent0705@gmail.com>
2024-06-30 15:16:45 +0800

    CICD TEST

--- controller/activity.js
+++ controller/activity.js
@@ -1,5 +1,6 @@
 const Activity = require("../models/activity.model");
 const User = require("../models/user.model");
+const redis = require("../services/redis");
 
 const handleErrors = (res, error) => {
     console.error(error);
@@ -8,7 +9,9 @@ const handleErrors = (res, error) => {
 
 const getActivity = async (req, res) => {
     try {
-        const { query: { page, pageSize, area, partnerEducationStep, isGrouping, category } } = req;
+        let { query: { page = 1, pageSize = 30, area, partnerEducationStep, isGrouping, category } } = req;
+        page = parseInt(page, 10);
+        pageSize = parseInt(pageSize, 10);
         const id = req.params.id;
         const userId = req.params.userId;
 
@@ -20,26 +23,40 @@ const getActivity = async (req, res) => {
         if (isGrouping) filter.isGrouping = isGrouping;
         if (category) filter.category = category;
 
+        // 创建缓存键，只考虑过滤条件，不包括分页信息
+        const cacheKey = `activities:${JSON.stringify(filter)}`;
+        const cachedData = await redis.get(cacheKey);
+        let allData = cachedData ? JSON.parse(cachedData) : [];
+
+        // 计算从缓存中提取数据的起始位置和结束位置
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+
+        // 如果缓存中没有足够的数据，从数据库中查询并更新缓存
+        if (allData.length < endIndex) {
+            const additionalData = await Activity.find(filter)
+                .skip(allData.length)
+                .limit(30); // 每次查询30条数据
+
+            allData = allData.concat(additionalData);
+            await redis.set(cacheKey, JSON.stringify(allData), 'EX', 3600); // 更新缓存
+        }
+
+        // 提取需要的数据范围
+        const pagedData = allData.slice(startIndex, endIndex);
         const totalCount = await Activity.countDocuments(filter);
         const totalPages = Math.ceil(totalCount / pageSize);
 
-        const activity = await Activity.find(filter)
-            .skip((page - 1) * pageSize)
-            .limit(pageSize);
-
-        const userIds = Array.from(new Set(activity?.map(item => item.userId)));
-        console.log('userIds',userIds);
-            
+        // 获取相关用户信息
+        const userIds = Array.from(new Set(pagedData?.map(item => item.userId)));
         const users = await User.find({ _id: { $in: userIds } });
-        console.log('users',users);
 
-            
-        const combinedData = activity?.map(activityItem => {
+        // 组合活动和用户数据
+        const combinedData = pagedData?.map(activityItem => {
             const user = users?.find(userItem => userItem._id?.toString() === activityItem.userId?.toString());
-            console.log('combinedData_users',users)
             if (user) {
                 return {
-                    ...activityItem.toObject(), 
+                    ...((typeof activityItem.toObject === 'function') ? activityItem.toObject() : activityItem),
                     user: {
                         photoURL: user.photoURL,
                         name: user.name,
@@ -52,20 +69,19 @@ const getActivity = async (req, res) => {
             } else {
                 console.error('User not found for userId:', activityItem.userId);
                 return {
-                    ...activityItem.toObject(),
+                    ...((typeof activityItem.toObject === 'function') ? activityItem.toObject() : activityItem),
                     user: null,
                 };
             }
         });
 
-        console.log('combinedData',combinedData);
         res.json({
             data: combinedData,
             page,
             pageSize,
             totalCount,
             totalPages,
-        }); 
+        });
     } catch (error) {
         handleErrors(res, error);
     }
@@ -77,11 +93,11 @@ const createActivity = async (req, res) => {
         const existingActivity = await Activity.findOne({ title });
 
         if (existingActivity) {
-           res.json({message:"Activity already exists"});
+            res.json({ message: "Activity already exists" });
         } else {
             const newActivity = new Activity(req.body);
             const data = await newActivity.save();
-            res.json ({ data });
+            res.json({ data });
         }
     } catch (error) {
         handleErrors(res, error);
@@ -112,7 +128,7 @@ const deleteActivity = async (req, res) => {
         if (!deletedActivity) {
             return res.status(404).json({ error: "Activity not found" });
         }
-        res.json({deletedActivity});
+        res.json({ deletedActivity });
     } catch (error) {
         handleErrors(res, error);
     }
@@ -121,7 +137,7 @@ const deleteActivity = async (req, res) => {
 const changeActivityStatus = async (req, res) => {
     try {
         const _id = req.params.id;
-        console.log("changeActivityStatus",req.body);
+        console.log("changeActivityStatus", req.body);
         const changeActivityStatus = await Activity.findByIdAndUpdate(_id, req.body);
         if (!changeActivityStatus) {
             return res.status(404).json({ error: "Activity not found" });
@@ -133,7 +149,7 @@ const changeActivityStatus = async (req, res) => {
     } catch (error) {
         handleErrors(res, error);
     }
-}
+};
 
 module.exports = {
     createActivity,
--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,15 @@
-const User = require("../models/user.model")
+const User = require("../models/user.model");
+const redis = require("../services/redis");
+
 const getPartner = async (req, res, next) => {
     try {
-        let { query: { page, pageSize, educationStage,roleList, location, tag, search } } = req;
+        let { query: { page = 1, pageSize = 30, educationStage, roleList, location, tag, search } } = req;
+        page = parseInt(page, 10);
+        pageSize = parseInt(pageSize, 10);
         const _id = req.params.id;
 
         let filter = {};
-        if( _id) {
+        if (_id) {
             filter._id = _id;
         }
 
@@ -14,7 +18,7 @@ const getPartner = async (req, res, next) => {
             filter.educationStage = { $in: educationStage };
         }
         if (roleList) {
-            roleList =  roleList.split(',');
+            roleList = roleList.split(',');
             filter.roleList = { $in: roleList };
         }
 
@@ -24,56 +28,62 @@ const getPartner = async (req, res, next) => {
         }
 
         if (tag) {
-            tag =  tag.split(',');
-            console.log(tag)
+            tag = tag.split(',');
             filter.tagList = { $in: tag };
         }
         if (search) {
-            let searchQuery = { 
+            let searchQuery = {
                 $or: [
-                  { name: new RegExp(search, 'i') },
-                  { selfIntroduction: new RegExp(search, 'i') },
-                  { share: new RegExp(search, 'i') },
-                  { roleList: new RegExp(search, 'i') },
-                  { interestList: new RegExp(search, 'i') },
-                  { tagList: new RegExp(search, 'i') },
+                    { name: new RegExp(search, 'i') },
+                    { selfIntroduction: new RegExp(search, 'i') },
+                    { share: new RegExp(search, 'i') },
+                    { roleList: new RegExp(search, 'i') },
+                    { interestList: new RegExp(search, 'i') },
+                    { tagList: new RegExp(search, 'i') },
                 ]
-              };
+            };
             filter = searchQuery;
-            console.log(filter);
         }
-        
 
+        // 创建缓存键，只考虑过滤条件，不包括分页信息
+        const cacheKey = `partners:${JSON.stringify(filter)}`;
+        const cachedData = await redis.get(cacheKey);
+        let allData = cachedData ? JSON.parse(cachedData) : [];
+
+        // 计算从缓存中提取数据的起始位置和结束位置
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+
+        // 如果缓存中没有足够的数据，从数据库中查询并更新缓存
+        if (allData.length < endIndex) {
+            const additionalData = await User.find(filter)
+                .skip(allData.length)
+                .limit(30); // 每次查询30条数据
+
+            allData = allData.concat(additionalData);
+            await redis.set(cacheKey, JSON.stringify(allData), 'EX', 3600); // 更新缓存
+        }
+
+        // 提取需要的数据范围
+        const pagedData = allData.slice(startIndex, endIndex);
         const totalCount = await User.countDocuments(filter);
-
         const totalPages = Math.ceil(totalCount / pageSize);
 
-        console.log('users_filter',filter)
-
-        const users = await User.find(filter)
-            .skip((page - 1) * pageSize)
-            .limit(pageSize);
-
-        console.log('users',users)
-
-        console.log('res.json',{
-            data: users,
+        // 构建响应
+        const response = {
+            data: pagedData,
             page,
             pageSize,
             totalCount,
             totalPages,
-        })
-        res.json({
-            data: users,
-            page,
-            pageSize,
-            totalCount,
-            totalPages,
-        });
+        };
+
+        res.json(response);
     } catch (error) {
         next(error);
     }
 };
+
 const update = async (req, res, next) => {
     try {
         const _id = req.params.id;
